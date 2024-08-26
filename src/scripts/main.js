53f28d86c9a3fd114a5d082a6af42dee89648cab
Lucca <lucca07102002@gmail.com>
2024-08-25 21:25:08 -0300

    Alterado o estilo para: Aniversário

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const dataDoEvento = new Date("Oct 07, 2024 00:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
@@ -24,6 +24,6 @@ const contaAsHoras = setInterval(function() {
 
     if (distanciaAteOEvento < 0) {
         clearInterval(contaAsHoras);
-        document.getElementById('contador').innerHTML = '<b style="color: #f00;">Evento Expirado</b>';
+        document.getElementById('contador').innerHTML = '<b style="color: #4f2ac3">Já fiz aniversário ;)</b>';
     }
 }, 1000)
